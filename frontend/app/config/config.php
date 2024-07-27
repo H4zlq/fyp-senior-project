e816332e6d9ca61dc4f8edeaef84a230e08c830c
H4zlq <haziqnaim72@gmail.com>
2024-07-28 01:33:19 +0800

    refactor: Update .gitignore and frontend config file

---
+++ frontend/app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Base configuration
+ */
+const BASE_URL = 'PUT YOUR BASE URL HERE'; // e.g. http://localhost/fyp-project-damia/frontend/public
+const ADMIN_BASE_URL = 'PUT YOUR ADMIN BASE URL HERE'; // e.g. http://localhost/fyp-project-damia/admin
+const ADMIN_CONTROLLER = 'Admin';
+const CONTROLLER = 'Home';
+const ADMIN_METHOD = 'dashboard';
+const METHOD = 'index';
+
+/*
+ * Database configuration
+ */
+const DB_HOST = 'PUT YOUR DB HOST HERE'; // e.g. localhost
+const DB_USER = 'PUT YOUR DB USER HERE'; // e.g. root
+const DB_PASS = 'PUT YOUR DB PASSWORD HERE'; // e.g. root
+const DB_NAME = 'PUT YOUR DB NAME HERE'; // e.g. fyp_project_damia
